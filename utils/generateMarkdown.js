6a3183c339f5f8ea5cafe225033d8772c2066732
Axel Jamal <axeltaj@gmail.com>
2023-02-25 17:00:49 +0000

    Created generate markdown function and template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,38 @@
-// function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
 
+  ## License
+  ${data.license}
+  
+  ## Contributing
+  ${data.contribution}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  Please reachout to me at ${data.email} if you have any questions.
+  Also, feel free to view this project and others at https://github/${data.github}
 `;
 }
 
